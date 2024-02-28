64b94e52d161dabc38c072ca612b89de93bd4c64
Vishwa Gaurav <81325730+VishwaGauravIn@users.noreply.github.com>
2024-02-28 16:35:08 +0530

    navbar design

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,20 @@
-import React from 'react'
+import { IconHelp, IconPlus, IconSettings } from "@tabler/icons-react";
+import React from "react";
 
 export default function page() {
   return (
-    <div className='font-bold'>Hello India!</div>
-  )
+    <main>
+      <nav className="bg-[#121212] shadow-xl justify-between items-center flex w-full h-14 p-2 text-white">
+        <button className="opacity-75 hover:opacity-100 transition-all ease-in-out">
+          Done
+        </button>
+        <p className="">The Ocean</p>
+        <div className="flex gap-4">
+          <IconHelp className="stroke-1 w-7 h-7 opacity-75 hover:opacity-100 transition-all ease-in-out cursor-pointer" />
+          <IconSettings className="stroke-1 w-7 h-7 opacity-75 hover:opacity-100 transition-all ease-in-out cursor-pointer" />
+          <IconPlus className="stroke-1 w-7 h-7 opacity-75 hover:opacity-100 transition-all ease-in-out cursor-pointer" />
+        </div>
+      </nav>
+    </main>
+  );
 }
